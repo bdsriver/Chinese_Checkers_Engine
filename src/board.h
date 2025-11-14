e5dcc78d9013fe4c62afb99fdd46e7c17d7e5d3a
bdsriver <bendachuan@gmail.com>
2025-11-14 13:58:17 -0600

    added jumping move generation functionality

--- src/board.h
+++ src/board.h
@@ -10,11 +10,15 @@
 #define PLAYER_PIECE_AMOUNT 10
 #define SPACE_AMOUNT 121
 
+//access space (x,y) with startBoard[y][x]
 extern char startBoard[][BOARD_DIM];
+//access space (x,y) with inBounds[y][x]
 extern bool inBounds[][BOARD_DIM];
 extern std::unordered_map<int, std::pair<int,int>> bitToIndices;
-extern std::unordered_map<int, __uint128_t> moves;
-extern std::unordered_map<int, __uint128_t> jumps;
+extern std::vector<__uint128_t> moves;
+extern std::vector<__uint128_t> jumps;
+//mapping of the location of the end of the jump to the location of the intermediate space
+extern std::vector<std::unordered_map<int,int>> halfJumps;
 
 //refer to diagram for mapping explanation
 //must be called to set the global moves and jumps tables
@@ -26,7 +30,7 @@ void copyBoard(char original[][BOARD_DIM], char copy[][BOARD_DIM]);
 //return a vector of {space1, space2} pairs
 //occupied is a bitboard of piece locations on the board
 //pieces is a map of {<0-9>,<piece location>} for the player's piece
-std::vector<std::pair<int,int>> generateMoves(char board[][BOARD_DIM], __uint128_t occupied, std::vector<int> pieces);
+std::vector<std::pair<int,int>> generateMoves( __uint128_t occupied, std::vector<int> pieces);
 
 // return a vector with the locations of each piece
 std::vector<int> initPieces(char board[][BOARD_DIM], char playerID);
