7e2d014be68bc2105c692559ad32f77984a5ea36
James Sanders <jsszdd@umsystem.edu>
2025-12-17 17:01:23 -0600

    Added timing for each parallel function and printed to CSV

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,13 @@
 #include <random>
 #include <string>
 #include <algorithm>
+#include <chrono>
+#include <fstream>
 
 int main(){
+  std::ofstream csvFile("timing_results.csv");
+  csvFile << "Depth,Search,ThreadSearch,OMPSearch\n";
+
   srand(time(nullptr));
   setMovesAndJumps();
   Hash::initPieceVals(pieceHashValue);
@@ -26,17 +31,32 @@ int main(){
   float startEval = posEval(startPoints, 0,0);
   
   //make a for loop iterating depth from 0 to 100 instead of using
-  int depth = 50;
-  
-  TranspositionTable t = TranspositionTable();
-  //measure this function
-  SearchResult s = Search(&occupied,&pieces,SearchNode(-500,500,startEval,0,0,depth,hash),&t);
+  int limit = 100;
+  for (int depth=0; depth<=limit; depth++){
+    TranspositionTable t = TranspositionTable();
+    //measure this function
+    auto start = std::chrono::high_resolution_clock::now();
+    SearchResult s = Search(&occupied,&pieces,SearchNode(-500,500,startEval,0,0,depth,hash),&t);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    double timeSearch = std::chrono::duration<double>(end - start).count();
 
-  //measure this function
-  s = threadSearch(occupied,pieces,startEval,depth,0);
+    //measure this function
+    start = std::chrono::high_resolution_clock::now();
+    s = threadSearch(occupied,pieces,startEval,depth,0);
+    end = std::chrono::high_resolution_clock::now();
+    elapsed = end - start;
+    double timeThread = std::chrono::duration<double>(end - start).count();
+    
+    //measure this function
+    //start = std::chrono::high_resolution_clock::now();
+    //s = ompSearch(occupied,pieces,startEval,depth,0);
+    //end = std::chrono::high_resolution_clock::now();
+    //elapsed = end - start;
+    //timeOMP = std::chrono::duration<double>(end - start).count();
 
-  //measure this function
-  //s = ompSearch(occupied,pieces,startEval,depth,0);
-  
-
+    csvFile << depth << "," << timeSearch << "," << timeThread << "," << timeOMP << "\n";
+  }
+  csvFile.close();
+  return 0;
 }
