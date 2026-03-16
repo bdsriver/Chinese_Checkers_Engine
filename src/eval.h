d946c523ea7d125782143a3d0471a4838a0f441a
bdsriver <bendachuan@gmail.com>
2026-03-16 10:17:04 -0500

    does not work with players (pieces) removed

--- src/eval.h
+++ src/eval.h
@@ -60,6 +60,7 @@ struct Move{
 
 SearchResult ignorantSearch(__uint128_t *board, __uint128_t * pieces, SearchNode node,TranspositionTable* table);
 
+//Deprecated for now
 float posEval(std::vector<__uint128_t>* pieces, int currTurn);
 
 float moveVal(Move m);
